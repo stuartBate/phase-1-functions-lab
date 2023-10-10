5160553c7e5a7f0e0be13f98dad4c21a76356102
Stuart Bate <bate.stuart93@gmail.com>
2023-10-10 15:10:18 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+const hq = 42;
+
+function distanceFromHqInBlocks(street){
+    return Math.abs(hq - street);
+}
+
+function distanceFromHqInFeet(street){
+    return Math.abs(hq - street) * 264;
+}
+
+function distanceTravelledInFeet(start, destination){
+    return Math.abs(start - destination) * 264;
+}
+
+function calculatesFarePrice(start, destination){
+    const distance = distanceTravelledInFeet(start, destination);
+    if (distance < 400){
+        return 0
+    }else if (distance>= 400 && distance< 2000 ){
+        return .02 * (distance - 400);
+    }else if (distance >= 2000 && distance < 2500){
+        return 25
+    }else{
+        return `cannot travel that far`;
+    }
+}
